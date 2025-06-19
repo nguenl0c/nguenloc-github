f029d66739f8d185716c7297fbeaf96565aa239a
nguenl0c <locnguenphu@gmail.com>
2025-06-19 11:52:26 +0700

    123

--- src/components/StatusColumn.jsx
+++ src/components/StatusColumn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StatusColumn = () => {
   return (
     <div>
-      <div className='w-10 h-full bg-gray-200'></div>
+      <div className='w-32 h-full bg-gray-200'></div>
     </div>
   );
 };
