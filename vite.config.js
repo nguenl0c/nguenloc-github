96bb84f5dd7edab37f0aeb8952e79f0a83c8b22f
nguenl0c <locnguenphu@gmail.com>
2025-06-23 18:17:04 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/my-clone-github/",
   server: {
     port: 3001,
     host: true,
